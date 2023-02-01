e88023d2463ca93c56a68b59d0b03556d4a8fe01
AlexandraHamd <pylina.alexandra@yandex.ru>
2023-02-01 09:02:36 +0300

    HW

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,4 @@
-# from django.contrib.auth.views import LogoutView
-# from django.urls import path
-# from users.apps import UsersConfig
-# from users.views import UserLoginView, UserEditView, UserRegisterView
-#
-# app_name = UsersConfig.name
-#
-# urlpatterns = [
-#     path('', UserLoginView.as_view(), name='login'),
-#     path('logout/', LogoutView.as_view(), name='logout'),
-#     path('register/', UserRegisterView.as_view(), name='register'),
-#     path('profile/', UserEditView.as_view(), name='profile'),
-# ]
+
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
